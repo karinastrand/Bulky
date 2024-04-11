270ea4da6a121db1d0716723ca7a395058cec79d
karinastrand <karinastrand@yahoo.se>
2024-04-11 09:06:05 +0200

    backup

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category obj) 
+    {
+        if (ModelState.IsValid) 
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+       
+    }
+    public IActionResult Edit(int? id)
+    {
+        if (id==null || id==0) 
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb==null) 
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+
+    }
+}
